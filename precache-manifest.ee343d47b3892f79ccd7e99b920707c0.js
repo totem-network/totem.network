01d6ab5661b27d09b00bc6c46278c9ac71dca91b
Daniel Wagner <daniel.wagner@totem.network>
2019-01-13 12:28:57 +0100

    first commit

---
+++ precache-manifest.ee343d47b3892f79ccd7e99b920707c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d4ca9142d8e36b58f5f4eae45cfcf7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef36c30e351f148b13c8",
+    "url": "/bundle.js"
+  },
+  {
+    "revision": "b197ce124d69182fe3552bf0db1af5b8",
+    "url": "/assets/icon/logo_48x48.png"
+  },
+  {
+    "revision": "9dafb10afa5958118d7ed121744d21a0",
+    "url": "/assets/icon/logo_32x32.png"
+  },
+  {
+    "revision": "88c79d256492b6668a5b20f6f08481f9",
+    "url": "/assets/icon/logo_16x16.png"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.1fb7b6eb32a73677326d3c438c5e1bf2.js"
+);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(mp4|webm)$/, workbox.strategies.cacheFirst({ cacheName: "video-cache", plugins: [] }), 'GET');
+workbox.routing.registerRoute(/\.(png)$/, workbox.strategies.cacheFirst({ cacheName: "image-cache", plugins: [] }), 'GET');
+workbox.routing.registerRoute(/\.(woff|woff2)$/, workbox.strategies.cacheFirst({ cacheName: "font-cache", plugins: [] }), 'GET');
